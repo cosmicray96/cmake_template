58b0992a44080cd066eb707ae38210d6ff50296a
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-05-13 10:20:12 +0000

    initial commit

---
+++ cmake/add_targets.cmake
@@ -0,0 +1,63 @@
+
+function(add_public_sources target_name)
+	set(path_to_target "${CMAKE_SOURCE_DIR}/projects/${target_name}")
+	
+	file(GLOB_RECURSE target_sources_public 
+		"${path_to_target}/inc/${target_name}/*.h"
+	)
+	target_sources(${target_name} 
+		PUBLIC
+			${target_sources_public}
+	)	
+endfunction()
+
+
+function(add_private_sources target_name)
+	set(path_to_target "${CMAKE_SOURCE_DIR}/projects/${target_name}")
+	
+	file(GLOB_RECURSE target_sources_private 
+		"${path_to_target}/src/${target_name}/*.h"
+		"${path_to_target}/src/${target_name}/*.c"
+	)
+	target_sources(${target_name} 
+		PRIVATE
+			${target_sources_private}
+	)	
+endfunction()
+
+
+function(target_common target_name)
+	
+	set_target_properties(${target_name}
+		PROPERTIES
+			DEFINE_SYMBOL ""
+	)
+	
+	# This ensures symbols are hidden by default on GCC/Clang
+	set_target_properties(${target_name} PROPERTIES
+		C_VISIBILITY_PRESET hidden
+		VISIBILITY_INLINES_HIDDEN YES
+	)
+
+endfunction()
+
+
+function(add_static_target target_name)
+	
+	add_library(${target_name} STATIC)
+	target_common(${target_name})
+
+	add_public_sources(${target_name})
+	add_private_sources(${target_name})
+
+	set(path_to_target "${CMAKE_SOURCE_DIR}/projects/${target_name}")
+	target_include_directories(${target_name}
+		PUBLIC
+			"${path_to_target}/inc/"
+		PRIVATE
+			"${path_to_target}/src/"
+	)
+
+	message("Target Added: ${target_name} (StaticLibrary)")
+
+endfunction()
