510b03593a4cc59b8bf7248d6279d11006db9f83
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-08-09 15:20:22 +0000

    back to clangd

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -9,4 +9,12 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${bin_dir}")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${bin_dir}") 
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${junk_dir}") 
 
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+add_custom_target(copy_compile_commands ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_BINARY_DIR}/compile_commands.json
+        ${proj_root_dir}/compile_commands.json
+    COMMENT "Copying compile_commands.json to project root"
+    VERBATIM
+)
 
