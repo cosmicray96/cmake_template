b6b4ec685b58b5afa964896310ed6ce4cbaea01b
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-08-20 16:37:24 +0000

    version 0.0.1

--- cmake/main.cmake
+++ cmake/main.cmake
@@ -1,3 +1,6 @@
+
+message("Template Version: 0.0.1")
+
 include("${CMAKE_CURRENT_LIST_DIR}/feature.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/target.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/tests.cmake")
