dc60a1f17a6750a17747ea857d4338df12b9b704
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-09-20 09:53:12 +0000

    new version, added convinient functions

--- cmake/main.cmake
+++ cmake/main.cmake
@@ -1,5 +1,5 @@
 
-message("Template Version: 0.0.1")
+message("Template Version: 0.0.2")
 
 include("${CMAKE_CURRENT_LIST_DIR}/feature.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/target.cmake")
--- cmake/target.cmake
+++ cmake/target.cmake
@@ -73,3 +73,69 @@ function(target_defines_exe_private target_name out_var)
 	# space for more
 	set(${out_var} "${defines}" PARENT_SCOPE)
 endfunction()
+
+function(target_simple_exe target_name)
+	target_files_public(${target_name} pub_files)
+	target_files_private(${target_name} priv_files)
+	target_inc_dir_public(${target_name} pub_includes)
+	target_inc_dir_private(${target_name} priv_includes)
+	target_defines_exe_private(${target_name} priv_defs)
+
+	add_executable(${target_name}
+		${pub_files}
+		${priv_files}
+	)
+	target_include_directories(${target_name}
+		PUBLIC ${pub_includes}
+		PRIVATE ${priv_includes}
+	)
+	target_compile_definitions(${target_name}
+		PRIVATE ${priv_defs}
+	)
+endfunction()
+
+function(target_simple_static target_name)
+
+	target_files_public(${target_name} pub_files)
+	target_files_private(${target_name} priv_files)
+	target_inc_dir_public(${target_name} pub_includes)
+	target_inc_dir_private(${target_name} priv_includes)
+	target_defines_static_private(${target_name} priv_defs)
+	target_defines_static_public(${target_name} pub_defs)
+
+	add_library(${target_name} STATIC
+		${pub_files}
+		${priv_files}
+	)
+	target_include_directories(${target_name}
+		PUBLIC ${pub_includes}
+		PRIVATE ${priv_includes}
+	)
+	target_compile_definitions(${target_name}
+		PUBLIC ${pub_defs}
+		PRIVATE ${priv_defs}
+	)
+endfunction()
+
+function(target_simple_shared target_name)
+	target_files_public(${target_name} pub_files)
+	target_files_private(${target_name} priv_files)
+	target_inc_dir_public(${target_name} pub_includes)
+	target_inc_dir_private(${target_name} priv_includes)
+	target_defines_shared_private(${target_name} priv_defs)
+	target_defines_shared_public(${target_name} pub_defs)
+
+	add_library(${target_name} SHARED
+		${pub_files}
+		${priv_files}
+	)
+	target_include_directories(${target_name}
+		PUBLIC ${pub_includes}
+		PRIVATE ${priv_includes}
+	)
+	target_compile_definitions(${target_name}
+		PUBLIC ${pub_defs}
+		PRIVATE ${priv_defs}
+	)
+endfunction()
+
